cb940e64228686fa956ab6d15021b3720c416966
Patricio <patri10718@gmail.com>
2020-05-18 14:44:32 -0300

    Finish commit

---
+++ dist/js/vistas/vistaAdministrador.js
@@ -0,0 +1,109 @@
+/*
+ * Vista administrador
+ */
+var VistaAdministrador = function(modelo, controlador, elementos) {
+  this.modelo = modelo;
+  this.controlador = controlador;
+  this.elementos = elementos;
+  var contexto = this;
+
+  // suscripción de observadores
+  this.modelo.preguntaAgregada.suscribir(function() {
+    contexto.reconstruirLista();
+  });
+  this.modelo.preguntaEliminada.suscribir(function() {
+    contexto.reconstruirLista();
+  });
+  this.modelo.preguntaEditada.suscribir(function() {
+    contexto.reconstruirLista();
+  });
+  this.modelo.todoBorrado.suscribir(function() {
+    contexto.reconstruirLista();
+  });
+};
+
+
+VistaAdministrador.prototype = {
+  //lista
+  inicializar: function() {
+    //llamar a los metodos para reconstruir la lista, configurar botones y validar formularios
+    this.reconstruirLista();
+    this.configuracionDeBotones();
+    validacionDeFormulario();
+  },
+
+  construirElementoPregunta: function(pregunta){
+    var contexto = this;
+    var nuevoItem;
+  
+    //se asigna a nuevoitem un elemento li con clase "list-group-item", id "pregunta.id" y texto "pregunta.textoPregunta"
+    nuevoItem = $('<li class="list-group-item"></li>');
+    nuevoItem.attr("id", pregunta.id);
+    nuevoItem.attr("text", pregunta.textoPregunta);
+
+    var interiorItem = $('.d-flex');
+    var titulo = interiorItem.find('h5');
+    titulo.text(pregunta.textoPregunta);
+    interiorItem.find('small').text(pregunta.cantidadPorRespuesta.map(function(resp){
+      return " " + resp.textoRespuesta;
+    }));
+    nuevoItem.html($('.d-flex').html());
+    return nuevoItem;
+  },
+
+  reconstruirLista: function() {
+    var lista = this.elementos.lista;
+    lista.html('');
+    var preguntas = this.modelo.preguntas;
+    for (var i=0;i<preguntas.length;++i){
+      lista.append(this.construirElementoPregunta(preguntas[i]));
+    }
+  },
+
+  configuracionDeBotones: function(){
+    var e = this.elementos;
+    var contexto = this;
+
+    //asociacion de eventos a boton
+    e.botonAgregarPregunta.click(function() {
+      var value = e.pregunta.val();
+      var respuestas = [];
+
+      $('[name="option[]"]').each(function() {
+        var respuestaIngresada = $(this).val();
+        if (respuestaIngresada !== ""){
+          respuestas.push({
+            'textoRespuesta': respuestaIngresada,
+            'cantidad': 0
+          });
+        }
+      });
+      contexto.limpiarFormulario();
+      contexto.controlador.agregarPregunta(value, respuestas);
+    });
+
+    //Evento borrar pregunta 
+    e.botonBorrarPregunta.click(function() {
+      var id_preguntaBorrar = parseInt($('.list-group-item.active').attr('id'));
+      contexto.controlador.borrarPregunta(id_preguntaBorrar);
+    });
+
+    //evento editar pregunta
+    e.botonEditarPregunta.click(function() {
+      var id_preguntaEditar =  parseInt($('.list-group-item.active').attr('id'));
+      var nuevoTexto = prompt("Edita tu pregunta");
+      contexto.controlador.editarPregunta(id_preguntaEditar, nuevoTexto);
+    });
+
+    //Evento borrar todas las preguntas
+    e.borrarTodo.click(function() {
+      contexto.controlador.borrarTodasPreguntas();
+    })
+  },
+
+  limpiarFormulario: function(){
+    $('.form-group.answer.has-feedback.has-success').remove();
+  },
+};
+
+
